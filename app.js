22030a0c1776504885943add183bd77e708070fa
yakupkalayci <ykpklyc18@gmail.com>
2022-09-01 14:38:59 +0300

    template engine added

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
 const app = express();
 
 //MIDDLEWARES
 app.use(express.static("public"));
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");  
+
+//ROUTES
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "temp/index.html"));
+    res.render("index");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/add_post", (req, res) => {
+    res.render("add_post");
+});
+
+app.get("/post", (req, res) => {
+    res.render("post");
 });
 
 
