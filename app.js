372806518140e3e1fbae4eebce166e512cee6893
yakupkalayci <ykpklyc18@gmail.com>
2022-09-02 23:28:43 +0300

    update the post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const path = require("path");
 const ejs = require("ejs");
 
@@ -14,6 +15,9 @@ mongoose.connect("mongodb://localhost:/cleanblogDB");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride("_method", {
+    methods: ["GET", "POST"]
+}));
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");  
@@ -41,14 +45,30 @@ app.get("/add_post", (req, res) => {
     res.render("add_post");
 });
 
-app.get("/post", (req, res) => {
-    res.render("post");
-});
-
 app.post("/blogPost", async (req, res) => {
     await post.create(req.body);
     res.redirect("/");
 });
+
+app.get("/posts/edit/:id", async(req, res) => {
+    const Post = await post.findById(req.params.id);
+    res.render("edit_post", {
+        Post
+    });
+});
+
+app.put("/posts/:id", async (req, res) => {
+    const Post = await post.findById(req.params.id);
+    Post.title = req.body.title;
+    Post.subTitle = req.body.subTitle;
+    Post.author = req.body.author;
+    Post.article = req.body.article;
+    Post.save();
+
+    res.redirect(`/posts/${Post.id}`);
+
+});
+
 
 
 const port = 3000;
